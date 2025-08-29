5c1991e0939b4bcfd582e06535b13810e579b5a5
jake <jake@example.com>
2025-08-29 07:16:28 -0400

    helper functions and all styles in drop-down

--- slavatracks/__init__.py
+++ slavatracks/__init__.py
@@ -14,17 +14,6 @@ from sqlalchemy.exc import IntegrityError
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger()
 
-try:
-    DATABASE_URL = os.environ["DATABASE_URL"]
-    DATABASE_URL = DATABASE_URL.replace("postgresql://", "postgresql+psycopg://")
-    logger.info("DATABASE_URL modified for psycopg-3")
-except KeyError:
-    logger.error("Unable to get DATABASE_URL")
-
-engine = create_engine(DATABASE_URL)
-Base = declarative_base()
-Session = sessionmaker(bind=engine)
-
 STYLE_ATTRS = [
     "argentine_tango",
     "bachata",
@@ -50,81 +39,109 @@ STYLE_ATTRS = [
     "west_coast_swing",
 ]
 
-class Track(Base):
-    __tablename__ = 'tracks'
-    __table_args__ = (
-        UniqueConstraint('title', 'artist', name='_title_artist_uc'),
-    )
-    id = Column(Integer, primary_key=True)
-    title = Column(String)
-    artist = Column(String)
-    streaming_link = Column(String)
-    timestamp = Column(DateTime)
-    argentine_tango = Column(Boolean, default=False)
-    bachata = Column(Boolean, default=False)
-    bolero = Column(Boolean, default=False)
-    cha_cha = Column(Boolean, default=False)
-    east_coast_swing = Column(Boolean, default=False)
-    foxtrot = Column(Boolean, default=False)
-    hustle = Column(Boolean, default=False)
-    jive = Column(Boolean, default=False)
-    lindy_hop = Column(Boolean, default=False)
-    mambo = Column(Boolean, default=False)
-    merengue = Column(Boolean, default=False)
-    night_club_2_step = Column(Boolean, default=False)
-    paso_doble = Column(Boolean, default=False)
-    peabody = Column(Boolean, default=False)
-    quickstep = Column(Boolean, default=False)
-    rumba = Column(Boolean, default=False)
-    salsa = Column(Boolean, default=False)
-    samba = Column(Boolean, default=False)
-    tango = Column(Boolean, default=False)
-    viennese_waltz = Column(Boolean, default=False)
-    waltz = Column(Boolean, default=False)
-    west_coast_swing = Column(Boolean, default=False)
+def style_attr_to_checkbox(s):
+    s = s.replace("_", " ").title().replace(" ", "") + "Checkbox"
+    return s[0].lower() + s[1:]
 
-    def prettified_styles(self):
-        styles = []
-        for a in sorted(self.__dict__.keys()):
-            if a in STYLE_ATTRS:
-                if self.__dict__[a]:
-                    styles.append(a.replace("_", " ").title())
-        return ", ".join(styles)
+def style_attr_to_input_name(s):
+    return s.replace("_", "-")
+
+def style_attr_to_title(s):
+    return s.replace("_", " ").title()
+
+def style_attr_to_details_li(s):
+    return f'<li><label><input type="checkbox" v-model="{style_attr_to_checkbox(s)}" name="{style_attr_to_input_name(s)}" />{style_attr_to_title(s)}</label></li><li>'
+
+try:
+    DATABASE_URL = os.environ["DATABASE_URL"]
+    DATABASE_URL = DATABASE_URL.replace("postgresql://", "postgresql+psycopg://")
+    logger.info("DATABASE_URL modified for psycopg-3")
+except KeyError:
+    logger.error("Unable to get DATABASE_URL")
+    DATABASE_URL = None
+
+if DATABASE_URL:
+    engine = create_engine(DATABASE_URL)
+    Base = declarative_base()
+    Session = sessionmaker(bind=engine)
 
 
-    def to_dict(self):
-        return {
-            "id": self.id,
-            "title": self.title,
-            "artist": self.artist,
-            "streaming_link": self.streaming_link,
-            "timestamp": str(self.timestamp),
-            "prettified_styles": self.prettified_styles(),
-            "argentine_tango": bool(self.argentine_tango),
-            "bachata": bool(self.bachata),
-            "bolero": bool(self.bolero),
-            "cha_cha": bool(self.cha_cha),
-            "east_coast_swing": bool(self.east_coast_swing),
-            "foxtrot": bool(self.foxtrot),
-            "hustle": bool(self.hustle),
-            "jive": bool(self.jive),
-            "lindy_hop": bool(self.lindy_hop),
-            "mambo": bool(self.mambo),
-            "merengue": bool(self.merengue),
-            "night_club_2_step": bool(self.night_club_2_step),
-            "paso_doble": bool(self.paso_doble),
-            "peabody": bool(self.peabody),
-            "quickstep": bool(self.quickstep),
-            "rumba": bool(self.rumba),
-            "salsa": bool(self.salsa),
-            "samba": bool(self.samba),
-            "tango": bool(self.tango),
-            "viennese_waltz": bool(self.viennese_waltz),
-            "waltz": bool(self.waltz),
-            "west_coast_swing": bool(self.west_coast_swing),
-        }
 
-Base.metadata.create_all(engine, checkfirst=True)
+    class Track(Base):
+        __tablename__ = 'tracks'
+        __table_args__ = (
+            UniqueConstraint('title', 'artist', name='_title_artist_uc'),
+        )
+        id = Column(Integer, primary_key=True)
+        title = Column(String)
+        artist = Column(String)
+        streaming_link = Column(String)
+        timestamp = Column(DateTime)
+        argentine_tango = Column(Boolean, default=False)
+        bachata = Column(Boolean, default=False)
+        bolero = Column(Boolean, default=False)
+        cha_cha = Column(Boolean, default=False)
+        east_coast_swing = Column(Boolean, default=False)
+        foxtrot = Column(Boolean, default=False)
+        hustle = Column(Boolean, default=False)
+        jive = Column(Boolean, default=False)
+        lindy_hop = Column(Boolean, default=False)
+        mambo = Column(Boolean, default=False)
+        merengue = Column(Boolean, default=False)
+        night_club_2_step = Column(Boolean, default=False)
+        paso_doble = Column(Boolean, default=False)
+        peabody = Column(Boolean, default=False)
+        quickstep = Column(Boolean, default=False)
+        rumba = Column(Boolean, default=False)
+        salsa = Column(Boolean, default=False)
+        samba = Column(Boolean, default=False)
+        tango = Column(Boolean, default=False)
+        viennese_waltz = Column(Boolean, default=False)
+        waltz = Column(Boolean, default=False)
+        west_coast_swing = Column(Boolean, default=False)
+
+        def prettified_styles(self):
+            styles = []
+            for a in sorted(self.__dict__.keys()):
+                if a in STYLE_ATTRS:
+                    if self.__dict__[a]:
+                        styles.append(a.replace("_", " ").title())
+            return ", ".join(styles)
+
+
+        def to_dict(self):
+            return {
+                "id": self.id,
+                "title": self.title,
+                "artist": self.artist,
+                "streaming_link": self.streaming_link,
+                "timestamp": str(self.timestamp),
+                "prettified_styles": self.prettified_styles(),
+                "argentine_tango": bool(self.argentine_tango),
+                "bachata": bool(self.bachata),
+                "bolero": bool(self.bolero),
+                "cha_cha": bool(self.cha_cha),
+                "east_coast_swing": bool(self.east_coast_swing),
+                "foxtrot": bool(self.foxtrot),
+                "hustle": bool(self.hustle),
+                "jive": bool(self.jive),
+                "lindy_hop": bool(self.lindy_hop),
+                "mambo": bool(self.mambo),
+                "merengue": bool(self.merengue),
+                "night_club_2_step": bool(self.night_club_2_step),
+                "paso_doble": bool(self.paso_doble),
+                "peabody": bool(self.peabody),
+                "quickstep": bool(self.quickstep),
+                "rumba": bool(self.rumba),
+                "salsa": bool(self.salsa),
+                "samba": bool(self.samba),
+                "tango": bool(self.tango),
+                "viennese_waltz": bool(self.viennese_waltz),
+                "waltz": bool(self.waltz),
+                "west_coast_swing": bool(self.west_coast_swing),
+            }
+
+    Base.metadata.create_all(engine, checkfirst=True)
 
 def tracks_to_json(session):
     return jsonify(sorted([t.to_dict() for t in session.query(Track).all()], key=lambda x: x["timestamp"]))
@@ -135,6 +152,10 @@ def create_app():
     @app.route('/')
     def index():
         return send_file('templates/index.html')  # no .j2 extension, so no jinja rendering here
+
+    @app.route('/favicon.ico')
+    def favicon():
+        return send_file('static/favicon.ico')
 
     @app.route("/tracks/")
     def get_tracks():
